9fc12d6cd21e82e0c0c17a5a3254f3178bc90440
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-17 00:01:11 +0530

    updated 4th script

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,7 +7,7 @@ PERSON2=$2
 
 # reference variables
 
-echo "$PERSON1:: Hello Suresh how are you?"
+echo "$PERSON1:: Hello $PERSON2 how are you?"
 echo "$PERSON2:: Hello $PERSON1 ,I am good. How are you?"
 echo "$PERSON1:: I am fine. How is your work?"
 echo "$PERSON2:: not bad. I am thinking take training on Devops"

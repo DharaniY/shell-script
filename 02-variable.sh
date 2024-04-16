d46709568ba691d32ae12c0fedfa68a40a37037a
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-16 23:51:48 +0530

    updated 2script

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 
-echo "Dharani parents are Sivamma and Satyanarayana."
-echo "Charan parents are Dharani and Jyothi."
+echo "Ramesh:: Hello Suresh how are you?"
+echo "Suresh:: Hello Ramesh ,I am good. How are you?"
+echo "Ramesh:: I am fine. How is your work?"
+echo "Suresh:: not bad. I am thinking take training on Devops"

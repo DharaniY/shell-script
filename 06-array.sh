978cfa2fec758a33e722a0aab1235b5ad25a1152
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-17 00:17:23 +0530

    added 6th script

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("Daddy" "RRR" "Love")
+
+echo "********************************"
+echo "first movie is ${MOVIES[0]}"
+echo "second movie is ${MOVIES[1]}"
+echo "ALL movies are ${MOVIES[@]}"

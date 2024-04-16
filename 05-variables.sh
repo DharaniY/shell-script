5196438803c13a42c869261c8cf9ea9b03e37dca
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-17 00:05:37 +0530

    added 5th script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username :: "
+
+read USERNAME  # here USERNAME is variable
+
+echo "Please enter password :: "
+
+read PASSWORD # here PASSWORD is variable
+
+echo "USERNAME is :: ${USERNAME}" and PASSWORD is :: ${PASSWORD}"

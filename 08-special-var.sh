a659b7ef840bfa1110d08f72ca129fba2c9f7299
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-17 00:27:28 +0530

    added 8th script

---
+++ 08-special-var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Print all variables::::::::: $@" # To print all variables
+echo "No of variables passed::::::::: $#" # To print number of variables passed
+echo "Script Name :::::::::::$0" # To print script name
+echo "Current working directory  :::::::::$PWD" # To print current working directory
+echo "Home Directory of current user  :::::::::$HOME" # To print

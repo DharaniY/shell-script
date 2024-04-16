ef19fdcbb03adcc7c70ee57bafbdb8041b68ccef
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-17 00:33:26 +0530

    modified 8th script

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -6,4 +6,5 @@ echo "Script Name :::::::::::$0" # To print script name
 echo "Current working directory  :::::::::$PWD" # To print current working directory
 echo "Home Directory of current user  :::::::::$HOME" # To print home directory of current user
 echo "Which User is running this script :::::::::$USER" # To print user running script
-echo "Host Name :::::::::$HOST" # To print host name
+echo "Host Name :::::::::$HOSTNAME" # To print host name
+echo "Process ID of current shell script :::::::::$$" # To print process ID

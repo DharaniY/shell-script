fbd2adb1f2713cf4319c9903b32d4ab164da8443
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-17 08:38:38 +0530

    updated 10th script

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,7 +4,12 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root acces"
+    exit 1 # manually exit the script
 else
     echo "Your super user"
 fi    
 dnf install mysql -y
+
+# $? = to check the status of previous command
+# if it is 0 = success
+# other than 0 considered the as failure

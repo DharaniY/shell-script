39095213e1d2b0dd3621c186a2763cd720a061fc
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-20 11:05:25 +0530

    updated 11th script

--- 11-funtions.sh
+++ 11-funtions.sh
@@ -4,8 +4,10 @@ USERID=$(id -u) # extract username
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access"
+    exit 1 # exit if this condition fails
 else
     echo "You are super user"
 fi
+
 dnf install mysql -y
 echo "is script proccessing?"    

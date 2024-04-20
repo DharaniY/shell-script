56190c67d5f47ee77215c099fdd5e52cb41dc4fa
Yeggodi, Dharani [External] <Dharani.Yeggodi@externals.adidas.com>
2024-04-20 13:49:50 +0530

    14 th script

---
+++ 14-install-package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=($id -u)
+
+if [ $USERID -eq 0 ] 
+then
+    echo "You are not root user, please login as root user"
+    exit 1 # exit if not root user
+else
+    echo "You are root user"
+fi    
+
+echo "All packages: $@"
